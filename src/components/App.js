c6580313b1431276873ec79244597440702cb64e
Joseph <wamiti8711@gmail.com>
2023-01-23 16:59:31 -0500

    directors component

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,8 @@ function App() {
       
         <Route exact path="/actors" ><Actors/></Route>
         <Route exact path="/movies"> <Movies/></Route>
-        
+        <Route exact path="/directors"> <Directors/></Route>
+        <Route exact path="/"><Home/></Route> 
 
       </Switch>
       </div>;
